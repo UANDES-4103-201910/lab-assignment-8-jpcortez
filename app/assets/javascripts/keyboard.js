ea5b77c15a3511cf5ca9009cd466be9ce2252ead
jorge <jpcortez@miuandes.cl>
2019-05-08 22:30:09 -0400

    falta

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,19 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+	$('.key').click(function()
+		{
+			console.log($(this).text());
+
+			var showed = ""
+
+			if ($(this).text()=="Bksp"){
+				$('#onscreenkeys').text().slice(-1) ;
+			}
+			else {
+				showed =  $('#onscreenkeys').text() + $(this).text();
+			}
+	    $('#onscreenkeys').text(showed) ;
+
+		})
 });
