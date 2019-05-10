331af64636db6f75b5764dd980241bd9c442be8b
Jorge <jpcortez@miuandes.cl>
2019-05-09 23:54:39 -0400

    kqk

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -4,16 +4,55 @@ $(() => {
 	$('.key').click(function()
 		{
 			console.log($(this).text());
+			
+	//		var showed = ""
+//
+			if ($(this).text()=="Enter"){
+				var para = document.createElement("p");
+				var node = document.createTextNode(" ");
+				para.appendChild(node);
 
-			var showed = ""
-
-			if ($(this).text()=="Bksp"){
-				$('#onscreenkeys').text().slice(-1) ;
+				var element = document.getElementById("onscreenkeys");
+				element.appendChild(para);
 			}
+			else if ($(this).text()=="Bksp"){
+				var some = document.getElementById("onscreenkeys").value;
+				document.getElementById("onscreenkeys").text(some.substring(0,some.length-1)) ;
+			//	var para = document.createElement("p");
+			//	var node = document.createTextNode($(this).text());
+			//	para.appendChild(node);
+//
+//				var element = document.getElementById("onscreenkeys");
+//				element.appendChild(node);
+			}
+
+			else if ($(this).text()=="Accept" || $(this).text()=="Cancel" || $(this).text()=="Shift" ){
+				var node = document.createTextNode("");
+				var element = document.getElementById("onscreenkeys");
+				element.appendChild(node);
+			}
+
 			else {
-				showed =  $('#onscreenkeys').text() + $(this).text();
+				
+				var node = document.createTextNode($(this).text());
+				
+
+				var element = document.getElementById("onscreenkeys");
+				element.appendChild(node);
 			}
-	    $('#onscreenkeys').text(showed) ;
+//				showed = $('#onscreenkeys').text().slice(0,-1) ;
+//			}
+//			else if ($(this).text()=="Accept" || $(this).text()=="Cancel" || $(this).text()=="Shift"){
+//				showed =  $('#onscreenkeys').text();
+//			}
+//			else if ($(this).text()=="Enter"){
+//				showed =  $('#onscreenkeys').text() + "\n";
+//			}
+//
+//			else {
+//				showed =  $('#onscreenkeys').text() + $(this).text();
+//			}
+//	    $('#onscreenkeys').text(showed) ;
 
 		})
 });
